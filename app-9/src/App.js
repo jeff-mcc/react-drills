300b03e232c5d5fb116a2c98d1b779b4b4fec078
jeff-mcc <reffery@gmail.com>
2021-04-21 23:12:31 -0600

    completed app-9

--- app-9/src/App.js
+++ app-9/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import of specific components can be performed as seen below which can then be passed to the Switch section of the router as seen below, or alternatively separate functions can be written directly into the code here as seen below the App() function
+import Home from './components/Home'
+import Signup from './components/Signup'
+import Details from './components/Details'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+            <li><Link to="/details">Details</Link></li>
+          </ul>
+        </nav>
+      
+        <Switch>
+          <Route path="/signup"><Signup /></Route>
+          <Route path="/details"><Details /></Route>
+          <Route path="/"><Home /></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
+
+// function Home(){
+//   return <h2>This is the Home page</h2>
+// }
+
+// function Signup(){
+//   return <h2>This is the Signup page</h2>
+// }
+
+// function Details(){
+//   return <h2>This is the Details page</h2>
+// }
 
 export default App;
